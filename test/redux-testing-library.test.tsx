efe7602df80e6ce1d7a33069b11859035c3c0659
Jan Brockmeyer <jabro@outlook.com>
2019-05-28 00:20:57 +0200

    fix(readme): simplify the example

--- test/redux-testing-library.test.tsx
+++ test/redux-testing-library.test.tsx
@@ -3,18 +3,15 @@ import * as React from "react";
 import { render } from "../src/redux-testing-library";
 import { TodoApp, TodoActions, TodoSelectors } from "./example";
 
-describe("todo app", () => {
-  describe("when addTodo action is dispatched with a given text as payload", () => {
-    it("adds a todo item with the given text to the store", async () => {
-      const { store, waitForStoreChange } = render(<TodoApp.UI />, TodoApp.store);
+describe("when addTodo action is dispatched", () => {
+  it("adds todo item to the store", async () => {
+    const { store, waitForStoreChange } = render(<TodoApp.UI />, TodoApp.store);
 
-      store.dispatch(TodoActions.addTodo("Do homework!"));
+    store.dispatch(TodoActions.addTodo("Test your application"));
 
-      await waitForStoreChange(state => {
-        expect(TodoSelectors.getTodos(state)).toStrictEqual([
-          { id: 0, completed: false, text: "Do homework!" }
-        ]);
-      });
+    await waitForStoreChange(state => {
+      const todos = TodoSelectors.getTodos(state);
+      expect(todos[0].text).toBe("Test your application");
     });
   });
 });
